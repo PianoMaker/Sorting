52e6492280cf121909d9940a62b16f96e57831e5
DELL <DELL@DESKTOP-DDIL4QF>
2023-02-21 21:28:49 +0200

    Add project files.

---
+++ clocks.cpp
@@ -0,0 +1,42 @@
+﻿// Project15.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int start;
+    cin >> start;
+    for (int i=0; i<start; i++)
+    {
+
+        int n = 0, m=0;
+        if (time(NULL) % 2 == 1)
+        {
+            while (time(NULL) % 2 == 1)
+            {
+                n++;
+            }
+        }
+        else
+        {
+            while (time(NULL) % 2 == 0)
+            {
+                m++;
+            }
+        }
+        cout << n << endl;   //  або n % x, якщо необхідно отримати число в заданих межах
+    }
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ sorting numbers.cpp
@@ -0,0 +1,59 @@
+﻿//Є масив на n елементів, знайти найбільший елемент масива і якщо він- парний - відсортувати масива від малих до великих, якщо непарний - навпаки. 
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	/* крок 1. генерую масив випадкових чисел велечиною N */
+	srand(time(NULL));
+	const int N = 10;
+	int mass [N];
+	for (int i = 0; i < N; i++)
+		mass[i] = rand();
+
+	/* крок 2. шукаю найбільше число */
+	int max = mass[0];
+	for (int i = 0; i < N; i++)
+		if (max < mass[i]) max = mass[i];
+
+	
+	/* крок 3. сортування масива: якщо максимум - парний */
+	if (max % 2 == 0)
+	{
+		int temp;
+		for (int j = 0; j < N; j++)
+			for (int i = 0; i < (N-1); i++)
+			{
+				temp = mass[i];
+				if (mass[i] > mass[i + 1])
+				{
+					mass[i] = mass[i + 1];
+					mass[i + 1] = temp;
+				}
+			}
+		cout << "\nMaximum = " << max << ". The number is even. \nAll elements sorted in ascending order are following:" << endl;
+		for (int i = 0; i < N; i++)
+			cout << mass[i] << " ";
+	}
+	else 
+	{
+		int temp;
+		for (int j = 0; j < N; j++)
+			for (int i = 0; i < (N-1); i++)
+			{
+				temp = mass[i];
+				if (mass[i] < mass[i + 1])
+				{
+					mass[i] = mass[i + 1];
+					mass[i + 1] = temp;
+				}
+			}
+		cout << "\nMaximum = " << max << ". The number is odd. \nAll elements sorted in descending order are following:" << endl;
+		for (int i = 0; i < N; i++)
+			cout << mass[i] << " ";
+	}
+}
+
